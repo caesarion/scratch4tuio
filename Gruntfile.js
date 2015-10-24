8f8ea68ab2816d5a15064eb877cea2b59cae02a5
Jonas Neugebauer <jneug@zitmail.upb.de>
2015-10-25 00:21:41 +0200

    Removed grunt-contrib-concat and added grunt-browserify to build

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,23 +13,12 @@ module.exports = function(grunt) {
             ' * Licensed under the <%= pkg.license %> license\n' +
             ' */\n',
 
-        concat: {
+        browserify: {
             options: {
-                stripBanners: true,
-                banner: '<%= banner %>'
+                banner: '<%= banner %>',
             },
             dist: {
-                src: [
-                    'build-support/js/lib-header.js',
-                    'node_modules/osc/dist/osc.js',
-                    'node_modules/lodash/lodash.js',
-                    'node_modules/socket.io-client/socket.io.js',
-                    'src/tuio.js',
-                    'build-support/js/lib-footer.js',
-                    'build-support/js/ext-header.js',
-                    'src/extension.js',
-                    'build-support/js/ext-footer.js'
-                ],
+                src: [ 'src/index.js' ],
                 dest: '<%= pkg.name %>.js',
             },
         },
@@ -44,7 +33,7 @@ module.exports = function(grunt) {
                 banner: '<%= banner %>'
             },
             dist: {
-                src: '<%= concat.dist.dest %>',
+                src: '<%= pkg.name %>.js',
                 dest: '<%= pkg.name %>.min.js'
             },
         },
@@ -72,18 +61,18 @@ module.exports = function(grunt) {
     });
 
     // grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-browserify');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-compress');
 
-    grunt.registerTask('sbx', 'Create .sbx file', function() {
+    grunt.registerTask('sbx', 'Create .sbx file.', function() {
         var descr = grunt.file.readJSON('src/descriptor.json');
 
         var project = grunt.file.readJSON('build-support/sbx/project.json');
         project.info.savedExtensions[0].extensionName = descr.title;
-        project.info.savedExtensions[0].javascriptURL = descr.script_url;
-        project.info.savedExtensions[0].blockSpecs = descr.blocks['en'];
-        project.info.savedExtensions[0].menus = descr.menus['en'];
+        project.info.savedExtensions[0].javascriptURL = descr.javascriptURL;
+        project.info.savedExtensions[0].blockSpecs = descr.descriptors['en'].blocks;
+        project.info.savedExtensions[0].menus = descr.descriptors['en'].menus;
 
         grunt.file.write('_tmp/project.json', JSON.stringify(project, null, 2));
 
@@ -95,6 +84,6 @@ module.exports = function(grunt) {
         grunt.file.delete('_tmp');
     });
 
-    grunt.registerTask('default', ['concat','uglify','sbx']);
+    grunt.registerTask('default', ['browserify','uglify','sbx']);
 
 };
